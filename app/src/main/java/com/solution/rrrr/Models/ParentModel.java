ea1e3147c7e23c00a8aa69df521d9bcb60ed1496
abdulrahim13 <1abdulraheem13@gmail.com>
2021-11-08 10:33:30 -0800

    Initial commit

---
+++ app/src/main/java/com/solution/rrrr/Models/ParentModel.java
@@ -0,0 +1,16 @@
+package com.solution.rrrr.Models;
+
+public class ParentModel {
+    private String movieCategory;
+
+
+    public ParentModel(String movieCategory) {
+        this.movieCategory = movieCategory;
+    }
+    public String movieCategory() {
+        return movieCategory;
+    }
+
+
+
+}
